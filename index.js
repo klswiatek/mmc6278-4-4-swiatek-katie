e61ee1f3e2da277d92c768d132fdb62293be2a54
klswiatek <klswiatek@yahoo.com>
2022-06-05 23:18:36 -0400

    Remaining Guesses added

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ var words = [
     'mango'
   ]
 
-  var chooseRandomWord = function (array) {
+var chooseRandomWord = function (array) {
       return array[Math.floor(Math.random() * array.length)];
   }
 
   var chosenWord = chooseRandomWord(words);
-  console.log(chosenWord)
+  console.log(chosenWord) 
+
+//*Word to Guess
+var wordToGuess = () => {
+    for (var i=0; i<chosenWord.length; i++) {
+        wordToGuess.push("_");
+    }
+    return underScore; 
+}
+document.getElementById("word-to-guess").innerHTML = wordToGuess;
+//*Previous Word
+
+//*Inncorect Guesses
+
+
+//*Remaining Guesses
+var remainingGuessDisplay = 10;
+document.getElementById("remaining-guesses").innerHTML = remainingGuessDisplay;
+
